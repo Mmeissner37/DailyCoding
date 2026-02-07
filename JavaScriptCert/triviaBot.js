8ad1b64159b5344b75c39e27eb7e722066dbecb9
Michaela Meissner <m.p.meissner85@gmail.com>
2026-02-06 19:47:00 -0500

    Added JS Cert Trivia Bot code

---
+++ JavaScriptCert/triviaBot.js
@@ -0,0 +1,36 @@
+let greeting = "Hello! I'm your coding fun fact guide!";
+console.log(greeting);
+
+let botName = "Sheldon";
+let botLocation = "the Moon";
+let favoriteLanguage = "Gibberish";
+
+const nameLocation =
+  "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(nameLocation);
+
+const language =
+  "My favorite programming language is " + favoriteLanguage + ".";
+console.log(language);
+
+let codingFact =
+  "Did you know that Java and JavaScript are very different? " +
+  "My favorite is still " +
+  favoriteLanguage +
+  ".";
+console.log(codingFact);
+
+codingFact =
+  "Although " + favoriteLanguage + " is my favorite, Python is a close second.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is still really great!";
+console.log(codingFact);
+
+let goodBye =
+  "It was fun sharing these facts with you. Goodbye! - " +
+  botName +
+  " from " +
+  botLocation +
+  ".";
+console.log(goodBye);
