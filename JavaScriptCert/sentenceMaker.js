7bef2fef60394bc1f20bc9d29f7c46574454cc39
Michaela Meissner <m.p.meissner85@gmail.com>
2026-02-06 19:56:30 -0500

    JS Cert Sentence Maker WS

---
+++ JavaScriptCert/sentenceMaker.js
@@ -0,0 +1,49 @@
+let adjective = "tired";
+let noun = "cat";
+let verb = "run";
+let place = "beach";
+let adjective2 = "energized";
+let noun2 = "bed";
+
+const firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("First story: " + firstStory);
+
+adjective = "bamboozled";
+noun = "nun";
+verb = "scamper";
+place = "tree";
+adjective2 = "exhausted";
+noun2 = "chair";
+
+const secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("Second story: " + secondStory);
