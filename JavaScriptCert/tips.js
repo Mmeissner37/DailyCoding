864042e22d6ee4a627a82a176723ab2b1d237712
Michaela Meissner <m.p.meissner85@gmail.com>
2026-02-14 09:33:13 -0500

    more tips

--- JavaScriptCert/tips.js
+++ JavaScriptCert/tips.js
@@ -11,3 +11,22 @@ const name = "Alice";
 const age = 25;
 const message = `My name is ${name} and I am ${age} years old.`;
 console.log(message);
+
+//template literals also allow for multi-line strings without needing special characters:
+
+let poem = `Roses are red,
+Violets are blue,
+JavaScript is fun,
+And so are you.`;
+
+console.log(poem);
+
+//template literals allow embeded JavaScript expressions directly w/in the string:
+
+const song = "Bohemian Rhapsody";
+const score = 9.5;
+const highestScore = 10;
+const output = `One of my favorite songs is "${song}". I rated it ${
+  (score / highestScore) * 100
+}%.`;
+console.log(output);
